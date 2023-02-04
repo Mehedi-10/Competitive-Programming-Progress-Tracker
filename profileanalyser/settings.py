1397c24094ffbfd04b17dae1c53a425f5d2f5c5c
Mehedi Hasan Arafat <mehedihasanarafat10@gmail.com>
2023-02-04 21:53:44 +0600

    deploy try-4

--- profileanalyser/settings.py
+++ profileanalyser/settings.py
@@ -16,14 +16,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-n5f1p1)wxq-uzl6+=yy3_4*@r49(+2hygixttv-6jlfwriw$1r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
 
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'profileanalyser.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -89,7 +89,8 @@ import dj_database_url
 
 DATABASES = {
     'default':
-    dj_database_url.parse('postgres://programmingclub_user:Hx9qbviH9S0qQyQ8PprAPcKm3XzEovZK@dpg-cff4gfo2i3mg6pf1a9o0-a.oregon-postgres.render.com/programmingclub')
+        dj_database_url.parse(
+            'postgres://programmingclub_1_user:ukEhfHsoT8UAZy7OhbAlSkbwr62qHEBU@dpg-cff7nupa6gdma8it1vp0-a.oregon-postgres.render.com/programmingclub_1')
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -124,11 +125,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+# This setting tells Django at which URL static files are going to be served to the user.
+# Here, they well be accessible at your-domain.onrender.com/static/...
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATIC_ROOT= BASE_DIR / 'static'
 # MEDIA_URL = '/abcd/'
-MEDIA_ROOT = BASE_DIR
+# MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
